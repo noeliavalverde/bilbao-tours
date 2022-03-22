4f1bea660dc6e54b3836adcacff2c035259a8230
Noelia Valverde <noelia.valverde@gmail.com>
2022-03-22 20:25:27 +0100

    Borrado de logo y paginas de vue. Creadas HomepageTours y ToursListPage y enlazadas en app.vue y en router-index

--- front/src/App.vue
+++ front/src/App.vue
@@ -1,7 +1,7 @@
 <template>
   <div id="nav">
     <router-link to="/">Home</router-link>|
-    <router-link to="/about">About</router-link>
+    <router-link to="/tours">Tours</router-link>
   </div>
   <router-view />
 </template>
