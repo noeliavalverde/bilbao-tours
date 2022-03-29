cd334c84152159ae34cd50f855bc5184e24e9bf5
noeliavm <noelia.valverde@gmail.com>
2022-03-29 15:04:11 +0200

    hecho loadData FAKE en ToursListPage y en TourDetailPage

--- front/src/pages/tour-detail/TourDetailPage.vue
+++ front/src/pages/tour-detail/TourDetailPage.vue
@@ -1,11 +1,14 @@
 <template>
   <div class="tour-detail-page">
 
-  <h3>{{tours.tour_name}}</h3>
-  
-  <pre>{{$data}}</pre>
-    
+  <h2>Nombre ruta: {{tour_name}}</h2>
+  <h3>Descripción ruta: {{tour_description}}</h3>
+  <ul>
+    <li>filtros: {{filter}}</li>
+  </ul>
   </div>
+
+  <pre>{{$data}}</pre>
 </template>
 
 <script>
@@ -14,35 +17,40 @@ export default {
   name: 'TourDetailPage',
   data() {
     return {
-       tours: {"tour_name":"Ruta 1",
-                "tour_id":"tour001",
-                "tour_description":"Breve explicacion ruta 1",
-                "favorite_tour":true,
-                "completed":false,
-                "filters":["arquitecture", "industrial"],
-                "quarter":["abando", "casco", "deusto", "uribarri"],
-                "tour_stops":[{"stop_id": "stop001",
-                                       "stop_name":"punto 1",
-                                       "stop_description":"explicación del punto",
-                                       "before_picture":"url",
-                                       "after_picture":"url",
-                                       },
-                                       {"stop_id": "stop002",
-                                       "stop_name":"punto 2",
-                                       "stop_description":"explicación del punto",
-                                       "before_picture":"url",
-                                       "after_picture":"url",
-                                       },
-                ]
-                }
-               
+        tour: {}
     }
   },
   mounted() {
+    this.loadData()
     
   },
   methods: {
-    
+     loadData(){
+       this.tour = {
+       tour_name:"Ruta 1",
+       tour_id:"tour001",
+       tour_description:"Breve explicacion ruta 1",
+       favorite_tour:true,
+       completed:false,
+       filters:["arquitecture", "industrial"],
+       quarter:["abando", "casco", "deusto", "uribarri"],
+       tour_stops:[{"stop_id": "stop001",
+                    "stop_name":"punto 1",
+                    "stop_description":"explicación del punto",
+                    "before_picture":"url",
+                    "after_picture":"url",
+                     },
+                    {"stop_id": "stop002",
+                     "stop_name":"punto 2",
+                     "stop_description":"explicación del punto",
+                     "before_picture":"url",
+                     "after_picture":"url",
+                     },
+                ]
+          
+       
+    }
+     }
 
   },
 }
--- front/src/pages/tours-list/ToursListPage.vue
+++ front/src/pages/tours-list/ToursListPage.vue
@@ -18,55 +18,61 @@ export default {
   name: 'ToursListPage',
   data() {
     return {
-      tours: [{"tour_name":"Ruta 1",
-                "tour_id":"tour001",
-                "tour_description":"Breve explicacion ruta 1",
-                "favorite_tour":true,
-                "completed":false,
-                "filters":["arquitecture", "industrial"],
-                "quarter":["abando", "casco", "deusto", "uribarri"],
-                "tour_stops":[{"stop_id": "stop001",
-                                       "stop_name":"punto 1",
-                                       "stop_description":"explicación del punto",
-                                       "before_picture":"url",
-                                       "after_picture":"url",
-                                       },
-                                       {"stop_id": "stop002",
-                                       "stop_name":"punto 2",
-                                       "stop_description":"explicación del punto",
-                                       "before_picture":"url",
-                                       "after_picture":"url",
-                                       },
-                ]
-                },
-                {"tour_name":"Ruta 2",
-                "tour_id":"tour002",
-                "tour_description":"Breve explicacion ruta 2",
-                "favorite_tour":true,
-                "completed":false,
-                "filters":["arquitecture", "industrial"],
-                "quarter":["abando", "casco", "deusto", "uribarri"],
-                "tour_stops":[{"stop_id": "stop001",
-                                       "stop_name":"punto 1",
-                                       "stop_description":"explicación del punto",
-                                       "before_picture":"url",
-                                       "after_picture":"url",
-                                       },
-                                       {"stop_id": "stop002",
-                                       "stop_name":"punto 2",
-                                       "stop_description":"explicación del punto",
-                                       "before_picture":"url",
-                                       "after_picture":"url",
-                                       },
-                ]
-                },
-               ]
+      tours: []
     }
   },
   mounted() {
-    
+    this.loadData()
   },
   methods: {
+
+    loadData(){
+        this.tours = [
+          {"tour_name":"Ruta 1",
+          "tour_id":"tour001",
+          "tour_description":"Breve explicacion ruta 1",
+          "favorite_tour":true,
+          "completed":false,
+          "filters":["arquitecture", "industrial"],
+          "quarter":["abando", "casco", "deusto", "uribarri"],
+          "tour_stops":[{"stop_id": "stop001",
+                         "stop_name":"punto 1",
+                         "stop_description":"explicación del punto",
+                         "before_picture":"url",
+                         "after_picture":"url",
+                         },
+                       {"stop_id": "stop002",
+                        "stop_name":"punto 2",
+                        "stop_description":"explicación del punto",
+                        "before_picture":"url",
+                        "after_picture":"url",
+                                       },
+                        ]
+                },
+          {"tour_name":"Ruta 2",
+           "tour_id":"tour002",
+          "tour_description":"Breve explicacion ruta 2",
+          "favorite_tour":true,
+          "completed":false,
+          "filters":["arquitecture", "industrial"],
+          "quarter":["abando", "casco", "deusto", "uribarri"],
+          "tour_stops":[{"stop_id": "stop001",
+                         "stop_name":"punto 1",
+                         "stop_description":"explicación del punto",
+                         "before_picture":"url",
+                         "after_picture":"url",
+                       },
+                       {"stop_id": "stop002",
+                        "stop_name":"punto 2",
+                        "stop_description":"explicación del punto",
+                        "before_picture":"url",
+                        "after_picture":"url",
+                       },
+                      ]
+          },
+               ]
+    }
+  
     
   },
 }
