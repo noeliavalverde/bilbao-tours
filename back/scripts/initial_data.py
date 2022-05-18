a7bd2e23e7bdbe1dd8f10f8b3660b0ed99d38341
noeliavm <noelia.valverde@gmail.com>
2022-05-18 14:59:10 +0200

    FEAT - Añadidos tour stops en initila data

--- back/scripts/initial_data.py
+++ back/scripts/initial_data.py
@@ -5,6 +5,7 @@ def main():
 
     from src.domain.info import Info, InfoRepository
     from src.domain.tour import Tour, TourRepository
+    from src.domain.stops import TourStop, TourStopRepository
 
     database_path = "data/database.db"
 
@@ -21,7 +22,7 @@ def main():
         completed=False,
         filters=["arquitecture", "history", "monuments"],
     )
-    tour_example2= Tour(
+    tour_example2 = Tour(
         tour_id="tour_002",
         tour_name="Segundo tour",
         tour_desc="Tour de ejemplo 2",
@@ -31,9 +32,28 @@ def main():
         filters=["nature", "history"],
     )
 
+    tour_stop_example = TourStop(
+        stop_id="001",
+        stop_name="Ejemplo de primera parada",
+        stop_description="Esta es un ejemplo de descripción de la primera parada",
+        before_picture="esto debe ser una url con imagen pasado",
+        after_picture="esto debe ser una url con imagen presente",
+    )
+
+    tour_stop_example2 = TourStop(
+        stop_id="002",
+        stop_name="Ejemplo de segunda parada",
+        stop_description="Esta es un ejemplo de descripción de la segunda parada",
+        before_picture="esto debe ser una url con imagen pasado",
+        after_picture="esto debe ser una url con imagen presente",
+    )
+
     tour_repository = TourRepository(database_path)
+    stop_repository = TourStopRepository(database_path)
     tour_repository.save_tour(tour_example)
     tour_repository.save_tour(tour_example2)
+    stop_repository.save_tour_stop(tour_stop_example)
+    stop_repository.save_tour_stop(tour_stop_example2)
 
 
 if __name__ == "__main__":
