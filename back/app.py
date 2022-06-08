fe8925cc9753862c1d2a33b47b877710eb540231
noeliavm <noelia.valverde@gmail.com>
2022-06-08 23:03:25 +0200

    REFACTOR - Pasado tour_stops a repositorio tours

--- back/app.py
+++ back/app.py
@@ -1,14 +1,12 @@
 import sqlite3
 from src.webserver import create_app
 from src.domain.tour import TourRepository
-from src.domain.stops import TourStopRepository
 
 
 database_path = "data/database.db"
 
 repositories = {
     "tours": TourRepository(database_path),
-    "tour_stops": TourStopRepository(database_path),
 }
 
 app = create_app(repositories)
--- back/scripts/initial_data.py
+++ back/scripts/initial_data.py
@@ -3,8 +3,7 @@ def main():
 
     sys.path.insert(0, "")
 
-    from src.domain.tour import Tour, TourRepository
-    from src.domain.stops import TourStop, TourStopRepository
+    from src.domain.tour import Tour, TourRepository, TourStop
 
     database_path = "data/database.db"
 
@@ -14,7 +13,6 @@ def main():
         tour_desc="A principios del siglo XIX la Villa de Bilbao salta la Ría. Las estrechas calles del Casco Viejo se han quedado pequeñas y se diseña una nueva ciudad moderna y luminosa. La burguesía quiere reflejarse en Europa y los modelos a seguir son París y Londres.",
         tour_front_image="https://www.vigoe.es/wp-content/uploads/2021/11/bilbao.jpg",
         favourite_tour=False,
-        completed=False,
         filters=["arquitecture", "history", "monuments"],
     )
     tour_example2 = Tour(
@@ -23,12 +21,11 @@ def main():
         tour_desc="Tour de ejemplo 2",
         tour_front_image="https://www.bilbao.bi/wp-content/uploads/2021/11/que-ver-en-bilbao-2.jpg",
         favourite_tour=False,
-        completed=False,
         filters=["nature", "history"],
     )
 
     tour_stop_example = TourStop(
-        stop_id="001",
+        stop_id="stop_001",
         stop_name="Ejemplo de primera parada",
         stop_description="Esta es un ejemplo de descripción de la primera parada",
         before_picture="esto debe ser una url con imagen pasado",
@@ -36,7 +33,7 @@ def main():
     )
 
     tour_stop_example2 = TourStop(
-        stop_id="002",
+        stop_id="stop_002",
         stop_name="Ejemplo de segunda parada",
         stop_description="Esta es un ejemplo de descripción de la segunda parada",
         before_picture="esto debe ser una url con imagen pasado",
@@ -44,7 +41,7 @@ def main():
     )
 
     tour_stop_example3 = TourStop(
-        stop_id="003",
+        stop_id="stop_003",
         stop_name="Ejemplo de tercera parada",
         stop_description="Esta es un ejemplo de descripción de la tercera parada",
         before_picture="esto debe ser una url con imagen pasado",
@@ -52,7 +49,7 @@ def main():
     )
 
     tour_stop_example4 = TourStop(
-        stop_id="004",
+        stop_id="stop_004",
         stop_name="Ejemplo de cuarta parada",
         stop_description="Esta es un ejemplo de descripción de la cuarta parada",
         before_picture="esto debe ser una url con imagen pasado",
@@ -60,7 +57,7 @@ def main():
     )
 
     tour_stop_example5 = TourStop(
-        stop_id="005",
+        stop_id="stop_005",
         stop_name="Ejemplo de quinta parada",
         stop_description="Esta es un ejemplo de descripción de la quinta parada",
         before_picture="esto debe ser una url con imagen pasado",
@@ -68,7 +65,7 @@ def main():
     )
 
     tour_stop_example6 = TourStop(
-        stop_id="006",
+        stop_id="stop_006",
         stop_name="Ejemplo de sexta parada",
         stop_description="Esta es un ejemplo de descripción de la sexta parada",
         before_picture="esto debe ser una url con imagen pasado",
@@ -76,15 +73,16 @@ def main():
     )
 
     tour_repository = TourRepository(database_path)
-    stop_repository = TourStopRepository(database_path)
+
     tour_repository.save_tour(tour_example)
     tour_repository.save_tour(tour_example2)
-    stop_repository.save_tour_stop(tour_stop_example)
-    stop_repository.save_tour_stop(tour_stop_example2)
-    stop_repository.save_tour_stop(tour_stop_example3)
-    stop_repository.save_tour_stop(tour_stop_example4)
-    stop_repository.save_tour_stop(tour_stop_example5)
-    stop_repository.save_tour_stop(tour_stop_example6)
+    tour_repository.save_tour_stop(tour_stop_example)
+    tour_repository.save_tour_stop(tour_stop_example2)
+    tour_repository.save_tour_stop(tour_stop_example3)
+    tour_repository.save_tour_stop(tour_stop_example4)
+    tour_repository.save_tour_stop(tour_stop_example5)
+    tour_repository.save_tour_stop(tour_stop_example6)
+    tour_repository.save_tour_stops_to_tour("tour_001", ["stop_006", "stop_002"])
 
 
 if __name__ == "__main__":
