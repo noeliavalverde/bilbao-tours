23cc9d409538e541908fe80e2e17d42e6d82d919
noeliavm <noelia.valverde@gmail.com>
2022-06-14 12:39:41 +0200

    FEAT - AdminPage con dashboard de administrador para ver rutas existentes, modificar, borrar o crear nuevas y pagina AdminDetail, para ver detalle de ruta existente y poder modificar

--- front/src/pages/admin/adminPage.vue
+++ front/src/pages/admin/adminPage.vue
@@ -2,7 +2,7 @@
   <div class="tours-list-page">
     <h1>Gestionar los tours</h1>
     
-    <button>Añadir nuevo tour</button>
+    <router-link to="/admin/manage-tours/add-tour">Añadir nuevo tour</router-link>
     
     <article class="tour-index-box" v-for="tour in tours" :key="tour.tour_id">
         
@@ -10,7 +10,7 @@
           <h3>Nombre del tour: {{tour.tour_name}}</h3>
           <p>Descripción del tour: {{tour.tour_desc}}</p>
           <button class="btn">Modificar</button>
-          <button class="btn">Eliminar</button>
+          <button class="btn" @click="removeTour(tour)">Eliminar</button>
           <router-link :to="`/admin/manage-tours/${tour.tour_id}`" class="btn">Ver detalle</router-link>
 
         </section>
@@ -21,6 +21,7 @@
 
 <script>
 import { getTours } from "@/services/api.js";
+import { deleteTour } from "@/services/api.js";
 
 export default {
   name: 'ToursListPage',
@@ -37,7 +38,14 @@ export default {
     async loadData(){
 
         this.tours = await getTours();
-    }
+    },
+
+    async removeTour(tour) {
+      console.log(tour)
+      this.tour = await deleteTour(tour);
+      location.reload(true);
+  
+  },
   
     
   },
