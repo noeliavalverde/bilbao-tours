73b16fd571fd35baead9c1e857168bf2e277bbde
noeliavm <noelia.valverde@gmail.com>
2022-06-23 18:31:55 +0200

    STYLE - añadido estilo a paginas de detalle

--- front/src/pages/admin-detail/AdminDetailPage.vue
+++ front/src/pages/admin-detail/AdminDetailPage.vue
@@ -1,12 +1,17 @@
 <template>
   <div class="tour-detail-page container">
-
-  
-  <h2>Nombre ruta: {{tour.tour_name}}</h2>
-  <h3>Descripción ruta: {{tour.tour_desc}}</h3>
-  <ul v-for="filter in tour.filters" :key="filter">
-    <li>filtros: {{filter}}</li>
-  </ul>
+  <header class="detail-header">
+    <router-link to="/admin/manage-tours" class="btn backlink">⇐ Volver a lista de tours</router-link>
+    
+    <h2>{{tour.tour_name}}</h2>
+    <h3>{{tour.tour_desc}}</h3>
+    <div class="filters-box">
+      <p>Filtros: </p>
+      <ul>
+        <li v-for="filter in tour.filters" :key="filter">{{filter}} </li>
+      </ul>
+    </div>
+  </header>
   
 
   <section v-for="stop in tour.stops" :key="stop.stop_id" class="stop-wrapper">
@@ -22,7 +27,14 @@
     </figure>
     
   </section>
-    <button>Modificar tour</button>
+   <nav>
+  <button class="btn">
+            <router-link :to="`/admin/manage-tours/modify-tour/${tour.tour_id}`">Modificar</router-link>
+          </button>
+          <button class="btn"><router-link :to="`/admin/manage-tours/add-tour-stops/${tour.tour_id}`">Añadir visitas</router-link></button>
+          <button class="btn remove-btn" @click="removeTour(tour)">Eliminar</button>
+          
+</nav>
   </div>
 
 </template>
@@ -31,6 +43,7 @@
 <script>
 
 import { getTourDetail } from "@/services/api.js";
+import { deleteTour } from "@/services/api.js";
 
 export default {
 
@@ -51,6 +64,12 @@ export default {
        this.tour = await getTourDetail(tourId)
 
      },
+     async removeTour(tour) {
+      
+      this.tour = await deleteTour(tour);
+      location.reload(true);
+  
+  },
 
   },
 }
@@ -58,30 +77,110 @@ export default {
 
 <style scoped>
 .container{
-  background-color: rgb(181, 178, 178);
+  background-color: rgb(242, 233, 233);
+  text-align: justify;
   
+}
+.tour-detail-page .backlink{
+  display: block;
+  margin-left: auto;
+  margin-bottom: 0.8em;
+  text-decoration: underline;
+  font-weight: 600;
+  font-size: 1.2em;
+  background-color: rgb(192, 197, 208);
+  color: rgba(32,12,70);
+}
 
+
+figure{
+  margin: 1.6em 0;
 }
-.stop-wrapper{
-  margin-bottom: 2em;
+figcaption{
+  padding: 1em;
+  background-color:rgb(6, 2, 46);
+  color: rgb(242, 233, 233);
+  font-weight: 500;
 }
+
 .detail-header h2{
-  font-weight: 400;
+  font-weight: 500;
   text-transform: uppercase;
   font-size: 1.6em;
   text-align: center;
   padding: 1em 0;
   background-color: rgb(6, 2, 46);
-  color: rgb(181, 178, 178);
+  color: rgb(242, 233, 233)
 }
 .detail-header h3{
   padding: 2em;
   font-weight: normal;
   font-style: italic;
+  margin: 0 auto;
+  max-width: 1000px;
+  
 }
 
-.detail-main{
-  padding: 2em;
+.filters-box{
+  max-width: 1000px;
+  margin: 0 auto 2em;
+  padding: 0 3em;
 }
+.filters-box ul{
+  display: flex;
+}
+.filters-box ul li{
+  margin-right: 1em;
+  padding-right: 1em;
+  border-right: 1px solid black;
+}
+
+.stop-wrapper{
+  padding: 2em;
+  width: 95%;
+  max-width: 800px;
+  margin: 0 auto;
+  box-shadow: 6px 6px 18px 3px rgba(40,40,41,0.61);
+  margin-bottom: 1em;
+}
+.stop-wrapper:nth-child(odd){
+  background-color: rgb(175, 184, 232);
+}
+.stop-wrapper:nth-child(even){
+  background-color: rgb(217, 221, 243);
+}
+.stop-wrapper h4{
+  margin-bottom: 1em;
+  text-align: center;  
+}
+
+nav{
+  padding: 2em 0 3em;
+  text-align: center;
+}
+nav *{
+  margin-right: 0.5em;
+}
+nav:last-child a{
+  margin-right: 0;
+}
+.remove-btn{
+  padding: 0.5em 0.7em;
+}
+.btn{
+  background-color: rgba(32,12,70);
+  color: rgb(192, 197, 208);
+  border: 1px solid rgba(32,12,70);
+  font-weight: bold;
+  
+}
+
+.btn:hover{
+  
+  background-color: rgb(192, 197, 208);
+  color: rgba(32,12,70);
+ 
+}
+
 
 </style>
--- front/src/pages/tour-detail/TourDetailPage.vue
+++ front/src/pages/tour-detail/TourDetailPage.vue
@@ -1,13 +1,11 @@
 <template>
   <div class="tour-detail-page container">
-
+   
     <header class="detail-header">
-      
+      <nav><router-link to="/tours" class="btn">⇐ Volver a lista de tours</router-link></nav>
       <h2> {{tour.tour_name}}</h2>
       <h3>{{tour.tour_desc}}</h3>
-      <ul v-for="filter in tour.filters" :key="filter">
-        <li>{{filter}}</li>
-      </ul>
+   
       
     </header>
 
@@ -51,49 +49,7 @@ export default {
      async loadData(){ 
        let tourId = this.$route.params.tour_id;
        this.tour = await getTourDetail(tourId)
-    //    this.tour = {
-    //    "tour_name":"Ruta 1",
-    //    "tour_id":"tour001",
-    //    "tour_description":"Breve explicacion ruta 1",
-    //    "favorite_tour":true,
-    //    "completed":false,
-    //    "tour_front_image": "https://www.vigoe.es/wp-content/uploads/2021/11/bilbao.jpg",
-    //    "filters":["arquitecture", "industrial"],
-    //    "quarter":["abando", "casco", "deusto", "uribarri"],
-    //    "tour_stops":[{"stop_id": "stop001",
-    //                      "stop_name":"Plaza Circular",
-    //                      "stop_description":"Don Diego López de Haro, Señor de Bizkaia, funda la villa en 1300, y aún la vigila desde lo alto de su monolito en la Plaza Circular rodeado del centro de negocios de esta pujante ciudad. Los tilos dan sombra y perfume a la Gran Vía invitándonos a pasear, a mirar tiendas y a admirar fachadas.",
-    //                      "before_picture":"https://i.pinimg.com/736x/a0/c6/57/a0c657021b83d2edd1c7c26d59c695a9--basque-country-bilbao.jpg",
-    //                      "before_figcaption":"Plaza circular - antes",
-    //                      "before_alt_text":"Plaza circular",
-    //                      "after_picture":"https://abraseguridad.es/wp-content/uploads/2017/12/Edificio-Plaza-Circular.jpg",
-    //                      "after_figcaption":"Plaza circular - actual",
-    //                      "after_alt_text":"Plaza circular",
-    //                      },
-    //                 {"stop_id": "stop002",
-    //                  "stop_name":"Alameda Urquijo/Bertendona",
-    //                  "stop_description":"Las sedes del Banco de España y del BBVA nos conducen hacia la Alameda Urquijo y a la Residencia, iglesia neogótica de piedra y ladrillo de los Jesuitas, entre fachadas eclécticas y miradores. Continuamos por Urquijo hasta encontrar a nuestra izquierda Bertendona. En esta calle descubrimos el Teatro Campos que parece tener vergüenza de ser diferente, de ser modernista. Contrasta con la rotunda monumentalidad de su vecino, el edificio de Correos, ejemplo de la arquitectura franquista, que sin complejos mira hacia Urquijo.",
-    //                  "before_picture":"https://scontent.fbio1-1.fna.fbcdn.net/v/t1.18169-9/970471_412930742161727_1705928822_n.jpg?_nc_cat=108&ccb=1-5&_nc_sid=9267fe&_nc_ohc=7Gg1qcGm7c4AX-5nFlS&_nc_ht=scontent.fbio1-1.fna&oh=00_AT-0-6rhE19UyW-tQj750z4aVj3HrIvjVPbUaesbC7vn7Q&oe=626B72EC",
-    //                  "before_figcaption":"Alameda Urquijo - antes",
-    //                  "before_alt_text":"Alameda Urquijo",
-    //                  "after_picture":"https://bilbaoarquitecturayurbanismo2ohome.files.wordpress.com/2019/11/p1080368.jpg",
-    //                  "after_figcaption":"Alameda Urquijo - actual",
-    //                  "after_alt_text":"Alameda Urquijo",
-    //                  },
-    //                  {"stop_id": "stop002",
-    //                  "stop_name":"Teatro Campos Elíseos",
-    //                  "stop_description":"El Teatro Campos Elíseos forma parte de la vida de Bilbao desde 1902, cuando se inauguró en lo que aquel entonces era el solar de los ‘Jardines de los Campos Elíseos’. Obra del arquitecto Alfredo Acebal y el decorador francés Jean Batiste Darroguy (quien diseñó su característica fachada), el edificio modernista está hoy catalagado como Bien de Interés Cultural. En plena expansión económica, urbanística y cultural de Bilbao, pronto el teatro, conocido ya como ‘la bombonera de Bertendona’, se convirtió en una de las salas de mayor categoría artística, y en la plataforma escénica que impulsó con mayor acierto la pujante ópera vasca, – por aquel entonces recién concebida. Además, cultivaba la programación dramática, cómica, y de cine, una función a la que terminó por dedicarse en exclusividad. Sin embargo, diversos infortunios, como el atentado que sufrió en 1978 llevaron al Teatro Campos Elíseos a su deterioro y al cese de su actividad. En 2010, un minucioso trabajo de rehabilitación consiguió recuperar en la medida de lo posible el diseño original modernista, a la vez que incrementó espacios y volúmenes. Actualmente la superficie del edificio es de 7300m2, donde conviven dos salas de representación y un centro de formación artístico y tecnológico. La modernización de sus instalaciones incluye además las soluciones tecnológicas más avanzadas para la representación de las artes escénicas. Desde 2017 el teatro está gestionado por Klemark Espectáculos Teatrales con un objetivo claro: devolver a la ciudad de Bilbao la riqueza artística y cultural que ha caracterizado durante años al Teatro Campos Elíseos. Un lugar donde las compañías nacionales e internacionales se den la mano con los artistas vascos, atendiendo siempre a la máxima calidad de sus espectáculos.",
-    //                  "before_picture":"https://i.pinimg.com/originals/3a/26/ce/3a26cef27dcea9e511b1ce2533b94529.jpg",
-    //                  "before_figcaption":"Teatro Campos Elíseos - antes",
-    //                  "before_alt_text":"Teatro Campos Elíseos",
-    //                  "after_picture":"https://images.adsttc.com/media/images/5128/a0e8/b3fc/4b11/a700/4a97/large_jpg/1284057850-mg-0006modb.jpg?1414370082",
-    //                  "after_figcaption":"Teatro Campos Elíseos - actual",
-    //                  "after_alt_text":"Teatro Campos Elíseos",
-    //                  },
-    //             ]
-          
-       
-    // }
+    
      },
 
   },
@@ -102,30 +58,66 @@ export default {
 
 <style scoped>
 .container{
-  background-color: rgb(181, 178, 178);
+  background-color: rgb(242, 233, 233);
+  text-align: justify;
   
+}
+.tour-detail-page nav a{
+  display: block;
+  margin-left: auto;
+  text-decoration: underline;
+  font-weight: 600;
+  font-size: 1.2em;
+}
 
+.tour-detail-page nav{
+  background-color: rgb(181, 178, 178);
 }
-.stop-wrapper{
-  margin-bottom: 2em;
+figure{
+  margin: 1.6em 0;
 }
+figcaption{
+  padding: 1em;
+  background-color:rgb(6, 2, 46);
+  color: rgb(242, 233, 233);
+  font-weight: 500;
+}
+
 .detail-header h2{
-  font-weight: 400;
+  font-weight: 500;
   text-transform: uppercase;
   font-size: 1.6em;
   text-align: center;
   padding: 1em 0;
   background-color: rgb(6, 2, 46);
-  color: rgb(181, 178, 178);
+  color: rgb(242, 233, 233)
 }
 .detail-header h3{
   padding: 2em;
   font-weight: normal;
   font-style: italic;
+  margin: 0 auto;
+  max-width: 1000px;
+  
 }
 
-.detail-main{
+.stop-wrapper{
   padding: 2em;
+  width: 95%;
+  max-width: 800px;
+  margin: 0 auto;
+  box-shadow: 6px 6px 18px 3px rgba(40,40,41,0.61);
+  margin-bottom: 1em;
+}
+.stop-wrapper:nth-child(odd){
+  background-color: rgb(175, 184, 232);
+}
+.stop-wrapper:nth-child(even){
+  background-color: rgb(217, 221, 243);
+}
+.stop-wrapper h4{
+  margin-bottom: 1em;
+  text-align: center;  
 }
 
 </style>
